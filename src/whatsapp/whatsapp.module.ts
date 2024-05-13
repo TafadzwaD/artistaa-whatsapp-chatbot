29067702cb1b1be7c4b690017c39590dda581656
TafadzwaD <tafadzademba@gmail.com>
2024-05-13 01:24:19 +0100

    add whatsapp controller and service

--- src/whatsapp/whatsapp.module.ts
+++ src/whatsapp/whatsapp.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { WhatsappController } from './whatsapp/whatsapp.controller';
+import { WhatsappService } from './whatsapp/whatsapp.service';
 
-@Module({})
+@Module({
+  controllers: [WhatsappController],
+  providers: [WhatsappService]
+})
 export class WhatsappModule {}
---
+++ src/whatsapp/whatsapp/whatsapp.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('whatsapp')
+export class WhatsappController {}
