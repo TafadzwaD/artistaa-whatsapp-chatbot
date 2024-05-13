5b5b2f726d5ef286ca01b792541d0a1871b51d88
TafadzwaD <tafadzademba@gmail.com>
2024-05-13 02:27:10 +0100

    Whatsapp verification endpoint.
    Authenticates our server endpoint

--- src/whatsapp/whatsapp/whatsapp.controller.ts
+++ src/whatsapp/whatsapp/whatsapp.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('whatsapp')
-export class WhatsappController {}
+export class WhatsappController {
+
+  @Get('webhook')
+  whatsappVerificationChallenge(@Req() request: Request){
+    const mode = request.query['hub.mode'];
+    const challenge = request.query['hub.challenge'];
+    const token = request.query['hub.verify_token'];
+
+    const verificationToken = process.env.WHATSAPP_CLOUD_API_WEBHOOK_VERIFICATION_TOKEN;
+
+    if(!mode || !token){
+      return 'Error verifying token';
+    }
+
+    if(mode === 'subscribe' && token === verificationToken){
+      return challenge?.toString();
+    }
+
+
+  }
+}
