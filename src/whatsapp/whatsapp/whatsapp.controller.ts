b388144d81b73fa5490ce8a9ba8f5fdd1868be56
TafadzwaD <tafadzademba@gmail.com>
2024-05-17 00:45:46 +0100

    refactor: linter

--- src/whatsapp/whatsapp/whatsapp.controller.ts
+++ src/whatsapp/whatsapp/whatsapp.controller.ts
@@ -3,23 +3,21 @@ import { Request } from 'express';
 
 @Controller('whatsapp')
 export class WhatsappController {
-
   @Get('webhook')
-  whatsappVerificationChallenge(@Req() request: Request){
+  whatsappVerificationChallenge(@Req() request: Request) {
     const mode = request.query['hub.mode'];
     const challenge = request.query['hub.challenge'];
     const token = request.query['hub.verify_token'];
 
-    const verificationToken = process.env.WHATSAPP_CLOUD_API_WEBHOOK_VERIFICATION_TOKEN;
+    const verificationToken =
+      process.env.WHATSAPP_CLOUD_API_WEBHOOK_VERIFICATION_TOKEN;
 
-    if(!mode || !token){
+    if (!mode || !token) {
       return 'Error verifying token';
     }
 
-    if(mode === 'subscribe' && token === verificationToken){
+    if (mode === 'subscribe' && token === verificationToken) {
       return challenge?.toString();
     }
-
-
   }
 }
