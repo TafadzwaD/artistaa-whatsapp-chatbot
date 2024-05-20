24cc30001bc993d884ab12660db96cf2fdcd3def
TafadzwaD <tafadzademba@gmail.com>
2024-05-20 18:10:38 +0100

    refactor: move code responsible for sending messages to user to WhatsappService.
    All logic should be handled there for easier extensibility - keeping the controller lean

--- src/whatsapp/whatsapp/whatsapp.controller.ts
+++ src/whatsapp/whatsapp/whatsapp.controller.ts
@@ -6,17 +6,15 @@ import {
   HttpCode,
   Post,
   Req,
-  Logger,
 } from '@nestjs/common';
 import { Request } from 'express';
-import { HttpService } from '@nestjs/axios';
+
 import * as process from 'node:process';
-import { catchError, lastValueFrom, map } from 'rxjs';
+import { WhatsappService } from './whatsapp.service';
 
 @Controller('whatsapp')
 export class WhatsappController {
-  private readonly httpService = new HttpService();
-  private readonly logger = new Logger(WhatsappController.name);
+  constructor(private readonly whatsAppService: WhatsappService) {}
 
   @Get('webhook')
   whatsappVerificationChallenge(@Req() request: Request) {
@@ -49,48 +47,11 @@ export class WhatsappController {
     switch (message.type) {
       case 'text':
         const text = message.text.body;
-        const url = `https://graph.facebook.com/${process.env.WHATSAPP_CLOUD_API_VERSION}/${process.env.WHATSAPP_CLOUD_API_PHONE_NUMBER_ID}/messages`;
-        const config = {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${process.env.WHATSAPP_CLOUD_API_ACCESS_TOKEN}`,
-          },
-        };
-        const data = JSON.stringify({
-          messaging_product: 'whatsapp',
-          recipient_type: 'individual',
-          to: messageSender,
-          type: 'text',
-          text: {
-            preview_url: false,
-            body: 'Response Message From Our Bot!!!',
-          },
-        });
 
-        try {
-          const response = this.httpService
-            .post(url, data, config)
-            .pipe(
-              map((res) => {
-                return res.data;
-              }),
-            )
-            .pipe(
-              catchError((error) => {
-                this.logger.error(error);
-                throw new BadRequestException(
-                  'Error Posting To WhatsApp Cloud API',
-                );
-              }),
-            );
-
-          const messageSendingStatus = await lastValueFrom(response);
-          this.logger.log('Message Sent. Status:', messageSendingStatus);
-        } catch (error) {
-          this.logger.error(error);
-          return 'Axle broke!! Abort mission!!';
-        }
+        await this.whatsAppService.sendWhatsAppMessage(messageSender);
         break;
     }
+
+    return 'Message processed';
   }
 }
--- src/whatsapp/whatsapp/whatsapp.service.ts
+++ src/whatsapp/whatsapp/whatsapp.service.ts
@@ -1,4 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
+import { catchError, lastValueFrom, map } from 'rxjs';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
-export class WhatsappService {}
+export class WhatsappService {
+  private readonly httpService = new HttpService();
+  private readonly logger = new Logger(WhatsappService.name);
+
+  async sendWhatsAppMessage(messageSender: string) {
+    const url = `https://graph.facebook.com/${process.env.WHATSAPP_CLOUD_API_VERSION}/${process.env.WHATSAPP_CLOUD_API_PHONE_NUMBER_ID}/messages`;
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.WHATSAPP_CLOUD_API_ACCESS_TOKEN}`,
+      },
+    };
+    const data = JSON.stringify({
+      messaging_product: 'whatsapp',
+      recipient_type: 'individual',
+      to: messageSender,
+      type: 'text',
+      text: {
+        preview_url: false,
+        body: 'Response Message From Our Bot!!!',
+      },
+    });
+
+    try {
+      const response = this.httpService
+        .post(url, data, config)
+        .pipe(
+          map((res) => {
+            return res.data;
+          }),
+        )
+        .pipe(
+          catchError((error) => {
+            this.logger.error(error);
+            throw new BadRequestException(
+              'Error Posting To WhatsApp Cloud API',
+            );
+          }),
+        );
+
+      const messageSendingStatus = await lastValueFrom(response);
+      this.logger.log('Message Sent. Status:', messageSendingStatus);
+    } catch (error) {
+      this.logger.error(error);
+      return 'Axle broke!! Abort mission!!';
+    }
+  }
+}
