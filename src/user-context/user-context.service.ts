74d19cc71470b6c6a651f46d77575158dbc88b26
TafadzwaD <tafadzademba@gmail.com>
2024-05-21 11:49:23 +0100

    save and retrieve chat history, Redis

--- src/user-context/user-context.service.ts
+++ src/user-context/user-context.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Redis } from 'ioredis';
 
 @Injectable()
-export class UserContextService {}
+export class UserContextService {
+  private readonly redis: Redis = new Redis(process.env.REDIS_URL || '');
+  private readonly logger: Logger = new Logger(UserContextService.name);
+
+  async saveToContext(
+    context: string,
+    contextType: 'user' | 'system',
+    userID: string,
+  ) {
+    try {
+      const value = JSON.stringify({
+        type: contextType,
+        data: { content: context },
+      });
+      await this.redis.rpush(userID, value);
+
+      return 'Context Saved!';
+    } catch (error) {
+      this.logger.error('Error Saving Context', error);
+      return 'Error Saving Context';
+    }
+  }
+
+  async getConversationHistory(userID: string) {
+    try {
+      const conversation = await this.redis.lrange(userID, 0, -1);
+      const jsonList = conversation.map((jsonString: string) => {
+        JSON.parse(jsonString);
+      });
+
+      return jsonList;
+    } catch (error) {
+      this.logger.error(error);
+      return [];
+    }
+  }
+}
