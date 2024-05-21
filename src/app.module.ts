8c59fd915023a16fe0b4b2006cf6b6fd7aef3316
TafadzwaD <tafadzademba@gmail.com>
2024-05-21 01:35:57 +0100

    create openai module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WhatsappModule } from './whatsapp/whatsapp.module';
 import { ConfigModule } from '@nestjs/config';
+import { OpenaiModule } from './openai/openai.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), WhatsappModule],
+  imports: [ConfigModule.forRoot(), WhatsappModule, OpenaiModule],
   controllers: [AppController],
   providers: [AppService],
 })
