a0b72cc062e1000986abea91bdcc1e30eedba576
TafadzwaD <tafadzademba@gmail.com>
2024-05-21 02:23:14 +0100

    use chat completions endpoint to generate AI response

--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { OpenAI } from 'openai';
 
 @Injectable()
-export class OpenaiService {}
+export class OpenaiService {
+  private readonly openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  private readonly logger = new Logger(OpenaiService.name);
+
+  async generateAIResponse(userInput: string) {
+    try {
+      const response = await this.openai.chat.completions.create({
+        messages: [{ role: 'user', content: userInput }],
+        model: process.env.OPENAI_MODEL || 'gpt-4o-2024-05-13',
+      });
+
+      return response;
+    } catch (error) {
+      this.logger.error('Error generating AI response', error);
+      // Fail gracefully!!
+      return 'Sorry, I am unable to process your request at the moment.';
+    }
+  }
+}
