d9698219778f436bfc2c53e8fb7e041c617b8c06
TafadzwaD <tafadzademba@gmail.com>
2024-05-21 12:16:14 +0100

    Register UserContext service in WhatsApp module

--- src/whatsapp/whatsapp.module.ts
+++ src/whatsapp/whatsapp.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { WhatsappController } from './whatsapp/whatsapp.controller';
 import { WhatsappService } from './whatsapp/whatsapp.service';
 import { OpenaiService } from 'src/openai/openai.service';
+import { UserContextService } from '../user-context/user-context.service';
 
 @Module({
   controllers: [WhatsappController],
-  providers: [OpenaiService, WhatsappService],
+  providers: [OpenaiService, WhatsappService, UserContextService],
 })
 export class WhatsappModule {}
