7749508f5e906ce05ea18cbeb45c866408267ed7
TafadzwaD <tafadzwademba@gmail.com>
2025-08-16 15:39:17 +0100

    refactor: remove redundant folder nesting

--- src/whatsapp/whatsapp.module.ts
+++ src/whatsapp/whatsapp.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { WhatsappController } from './whatsapp/whatsapp.controller';
-import { WhatsappService } from './whatsapp/whatsapp.service';
+import { WhatsappController } from './whatsapp.controller';
+import { WhatsappService } from './whatsapp.service';
 import { OpenaiService } from 'src/openai/openai.service';
 import { UserContextService } from '../user-context/user-context.service';
 import { StabilityaiService } from '../stabilityai/stabilityai.service';
